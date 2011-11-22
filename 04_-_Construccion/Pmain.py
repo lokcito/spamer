e45e9af1be3d530df3e1314ac3d0f08ee68a815a
lokcito <enciso.leo@gmail.com>
2011-11-22 02:18:25 -0500

    Cambios de controles de interfaz

--- 04_-_Construccion/Pmain.py
+++ 04_-_Construccion/Pmain.py
@@ -8,22 +8,25 @@ import gtk
 class Maincontrol:
 	def __init__(self):
 		self.builder = gtk.Builder()
-		self.builder.add_from_file("form_main.glade")
+		self.builder.add_from_file("Form_Main.glade")
 		self.window = self.builder.get_object("form")
-		self._btnConfiguracion = self.builder.get_object('btnConfiguracion')
+		#self._btnConfiguracion = self.builder.get_object('btnConfiguracion')
+		#icono = gtk.Image()
+		#icono.set_from_file("icono.png")
+		#self._btnConfiguracion.set_image(icono)
 		self.builder.connect_signals(self)
 		self.window.show_all()
 	
-	def click_btnConfiguracion(self, widget, data=None):
+	def click_toolbtnConfiguracion(self, widget, data=None):
 		print "Configurando"
 
-	def click_btnRecuperar(self, widget, data=None):
+	def click_toolbtnRecuperar(self, widget, data=None):
 		print "Editando"
 
-	def click_btnRedactar(self, widget, data=None):
+	def click_toolbtnMensaje(self, widget, data=None):
 		print "Redactando"
 	
-	def click_btnEnviar(self, widget, data=None):
+	def click_toolbtnEnviar(self, widget, data=None):
 		print "Enviando"
 
 if __name__ == "__main__":
